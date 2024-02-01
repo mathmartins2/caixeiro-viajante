4f826cd61301480afb68ebb0c3bed953d068092c
mathmartins2 <matheusgleiciel@gmail.com>
2024-02-01 11:50:15 -0300

    Add validation to CalculateRouteDto

--- back/src/customer/dto/calculate-route.dto.ts
+++ back/src/customer/dto/calculate-route.dto.ts
@@ -1,8 +1,16 @@
-import { ArrayMinSize, IsArray, IsNotEmpty } from 'class-validator';
+import {
+  ArrayMinSize,
+  ArrayNotEmpty,
+  IsArray,
+  IsNotEmpty,
+  IsUUID,
+} from 'class-validator';
 
 export class CalculateRouteDto {
   @IsNotEmpty()
   @IsArray()
   @ArrayMinSize(2)
+  @ArrayNotEmpty()
+  @IsUUID('4', { each: true })
   clientIds: string[];
 }
